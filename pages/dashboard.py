0cb715895ee6aaf812f57df9348ef643b90d7ff9
Long3309 <75237094+Long3309@users.noreply.github.com>
2023-06-11 14:31:06 +0700

    pre final deploy

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -7,7 +7,39 @@ st.set_page_config(layout="wide")
 st.header("CS313 - Final Project App")
 
 tongquan = pd.read_pickle("tongquan.pkl")
-fig = px.histogram(tongquan, x = "Diem_HP", text_auto=True, nbins = 20,
+fig = px.histogram(tongquan, x = "Diem_HP", text_auto=True, nbins = 50,
                    title= "Thông tin điểm thi sinh viên")
 fig.update_traces(textposition = "outside")
 st.plotly_chart(fig, use_container_width=True)
+
+# Chọn năm học muốn biết thông tin
+namhoc = tongquan["NamHoc"].unique()
+namchon = st.selectbox("CHỌN NĂM HỌC MUỐN BIẾT THÔNG TIN", options=sorted(namhoc))
+fig = px.histogram(tongquan.loc[tongquan["NamHoc"] == namchon],
+                   x = "Diem_HP", text_auto=True, nbins = 50,
+                   title= "Thông tin điểm thi sinh viên")
+fig.update_traces(textposition = "outside")
+st.plotly_chart(fig, use_container_width=True)
+
+# Các môn học có tỉ lệ rớt nhiều nhất
+ds_tilerot = pd.read_csv("ds_tilerot.csv")
+threshold = st.number_input("CHỌN THRESHOLD SỐ LƯỢNG ĐĂNG KÝ", min_value=500,max_value=10000, step=500, value=1000)
+fig = px.bar(ds_tilerot[ds_tilerot["Tổng số lượng"] > threshold].sort_values(["Tỉ lệ trượt"], ascending = False).head(10),
+             x = "MaMH", y = "Tỉ lệ trượt",
+             color="Tỉ lệ trượt", text_auto=True,
+             hover_data=["Số lượng rớt","Tổng số lượng"],
+             title= f"Top 10 môn có tỉ lệ trượt cao nhất (Số lượt đăng ký > {threshold})")
+fig.update_traces(textposition = "outside")
+st.plotly_chart(fig, use_container_width=True)
+
+# Chia môn học theo loại MH
+# tongquan_loaiMH = tongquan[["MaMH", "Loại MH"]]
+tongquan_loaiMH = pd.read_csv("soluong_MH.csv")
+# tongquan_loaiMH.groupby(["MaMH"]).count().reset_index("count")
+loaiMH = st.selectbox("CHỌN LOẠI MÔN HỌC", options=tongquan_loaiMH["Loại MH"].unique())
+fig = px.bar(tongquan_loaiMH.loc[tongquan_loaiMH["Loại MH"]== loaiMH],
+             x = "MaMH", y = "Tổng số lượng",
+             color = "Tổng số lượng",
+             title = f"Thông tin môn học thuộc Loại `{loaiMH}`")
+st.plotly_chart(fig, use_container_width=True)
+
